f947296bd41793587ce4e03d636618e697cb61ce
chakunyoung <chakunyou@naver.com>
2023-02-11 13:38:58 +0900

    아기상어 맞왜틀

--- Main.java
+++ Main.java
@@ -3,67 +3,16 @@ import java.io.*;
 
 import java.util.*;
 import java.io.*;
-// 경쟁상태에서 우선순위가 높은 상어가 이긴다.
 
-// 냄새가 없는 칸을 가장 우선순위
-// 다 냄새가 있다면 냄새가 있는 칸(자신의 냄새)를 사용함.
-// 자신의 냄새가 없다면 원래 우선순위 사용
-
-// 냄새는 k번 이동시 알아서 소멸
-
-// 1. 움직임 우선순위를 구현. // 객체마다 이중 리스트로 구현
-// 2. 냄새의 소멸을 구현. // 시간이 지나면 없어지는
-
-// 2차원 배열을 노드로 만들고
-// 큐에 노드를 x, y, time, shark number 를 이용해서 해당 위치를 update
 public class Main {
 
-	static Node[][] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int size = Integer.parseInt(st.nextToken());
-		int sharks = Integer.parseInt(st.nextToken());
-		int time = Integer.parseInt(st.nextToken());
-		
-		arr = new Node[size][size];
-		
-		for(int i = 0; i<size; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<size; j++) {
-				
-				
-			}
-		}
+			
 		
 		
 		
 		
-	}
-}
-
-class Node {
-	int x;
-	int y;
-	int time;
-	boolean isShark;
-	List<List<Integer>> move;
-
-	public Node(int x, int y, int time, boolean isShark) {
-		this.x = x;
-		this.y = y;
-		this.time = time;
-		this.isShark = isShark;
-
-		if(isShark) {
-			move = new ArrayList<>();
-			for (int i = 0; i < 4; i++)
-				move.add(new ArrayList<>());
-		}
-	}
-
-	public void moveSet(int m, int m1) {
-		move.get(m).add(m1);
 	}
 }
--- solved/simulation/_19237.java
+++ solved/simulation/_19237.java
@@ -2,27 +2,26 @@ package solved.simulation;
 
 import java.util.*;
 import java.io.*;
-// 경쟁상태에서 우선순위가 높은 상어가 이긴다.
-// 경쟁상태에서 순위 낮은 상어 제거
 
+/*
+  1. 구현해야할 액션(메서드) 정하기
+  2. 메서드 순서 정하기
+ */
 public class _19237 {
 	static Node[][] arr;
-	static List<SmellNode> smells = new LinkedList<>();
-	static List<SharkNode> sharks = new ArrayList<>();
+	static List<Node> smells = new LinkedList<>();
+	static List<SharkNode> sharks = new LinkedList<>();
 
-	static int k;
-	static int time;
-	static int finalSharkNumber = 0;
+	static int setTime;
+	static int answerTime;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int size = Integer.parseInt(st.nextToken());
 		int sharkCount = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		finalSharkNumber = sharkCount;
+		setTime = Integer.parseInt(st.nextToken());
 
-		// Node 배열 초기화
 		arr = new Node[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
@@ -30,145 +29,152 @@ public class _19237 {
 			}
 		}
 
-		// shark input
-		sharks.add(new SharkNode(0, 0, 0, 0));
-		
+		sharks.add(new SharkNode(-1, -1, -1, -1)); // index 0번 사용 안함
+
 		for (int i = 0; i < size; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < size; j++) {
-				int n = Integer.parseInt(st.nextToken());
-				if (n != 0) {
-					SharkNode sn = new SharkNode(i, j, n, 0);
-					arr[i][j] = sn;
-					sharks.add(sn);
+				int sharkNumber = Integer.parseInt(st.nextToken());
+				if (sharkNumber != 0) {
+					sharks.add(new SharkNode(i, j, sharkNumber, 0));
 				}
 			}
 		}
-		
-		Collections.sort(sharks, (o1, o2 ) -> o1.sharkNumber - o2.sharkNumber);
-		System.out.println(sharks);
-		
-		StringTokenizer stDir = new StringTokenizer(br.readLine());
-		for(int i = 1; i<sharks.size(); i++) 
-			sharks.get(i).dir = Integer.parseInt(stDir.nextToken()) - 1;
+		Collections.sort(sharks, (o1, o2) -> o1.sharkNumber - o2.sharkNumber);
 
-		// shark move set
+		StringTokenizer startSharkDir = new StringTokenizer(br.readLine());
+		for (int i = 1; i < sharks.size(); i++)
+			sharks.get(i).dir = Integer.parseInt(startSharkDir.nextToken()) - 1;
+
+		// shark set move
 		for (int i = 1; i < sharks.size(); i++) {
 			for (int j = 0; j < 4; j++) {
 				st = new StringTokenizer(br.readLine());
 				for (int k = 0; k < 4; k++)
-					sharks.get(i).setMove(j, Integer.parseInt(st.nextToken()) - 1);
+					sharks.get(i).setMoveList(j, Integer.parseInt(st.nextToken()) - 1);
 			}
 		}
-		// input end
 
 		q();
-		System.out.println(time);
+
+		System.out.println(answerTime);
 	}
 
 	public static void q() {
 		Queue<SharkNode> q = new LinkedList<>();
-		
-		for(int i = 1; i<sharks.size(); i++) 
+
+		// 초기 조건
+		for (int i = 2; i < sharks.size(); i++)
 			q.offer(sharks.get(i));
+		q.offer(sharks.get(1));
+		createSmellNode();
 
-		// 마지막 상어가 오면 1회전이 돈 것
 		while (!q.isEmpty()) {
-			SharkNode sn = q.poll();
-			// 움직일 빈 공간이 있는지 확인한다.
-			// 빈 공간으로 자신의 우선순위에 따라 이동한다.
-			// 없다면 자신의 smell로 이동한다.
-			if (!sharkMove(sn, 0))
-				sharkMove(sn, 1);
-			
-			q.offer(sn);
-			
-			//System.out.println(sharks + " --");
-			
-			// 1회전 종료 // time++; // smell을 남긴다. // smellLifeCycle - 1
-			if (sn.sharkNumber == finalSharkNumber) {
-				// obs 1 --
-				decrSmellNode();
-				removeShark();
-				createSmellNode();
-				time++;
-				
-				System.out.println(sharks);
-				
-				if(sharks.size() == 2)
-					return;
-				
-			}
-		} // end q
-	}
-	
-	public static void removeShark() {
-		for(int i = 1; i<sharks.size() - 1; i++) {
-			SharkNode sn1 = sharks.get(i);
-			for(int j = i+1; j<sharks.size(); j++) {
-				SharkNode sn2 = sharks.get(j);
-				if(sn1.x == sn2.x && sn1.y == sn2.y) {
-					if(sn1.sharkNumber > sn2.sharkNumber) 
-						sharks.remove(sn1);
-					else
-						sharks.remove(sn2);
+			SharkNode shark = q.poll();
+			if (!shark.survive)
+				continue;
+
+			// 상어 이동
+			if (!sharkMove(shark, 0))
+				sharkMove(shark, 1);
+
+			q.offer(shark);
+
+			// 1번 상어가 큐의 가장 마지막, 1회전 종료 조건
+			if (shark.sharkNumber == 1) {
+				decreaseSmellNode(); // 냄새 시간 감소
+				removeDeadShark(); // 상어 제거
+				// ->
+				createSmellNode(); // 현재 위치 냄새 생성
+
+				for (Node[] nn : arr) {
+					System.out.println(Arrays.toString(nn));
 				}
+				System.out.println(smells);
+				System.out.println();
+
+				System.out.println(answerTime);
+				
+				answerTime++;
+				if (answerTime > 1000) {
+					answerTime = -1;
+					return;
+				}
+
+				if (sharks.size() == 2)
+					return;
 			}
-		}
-	}
-	
-	
-	// 상어의 현재 위치에 생성
-	public static void createSmellNode() {
-		for (SharkNode sn : sharks) {
-			SmellNode smn = new SmellNode(sn.x, sn.y, sn.sharkNumber, k);
-			arr[sn.x][sn.y] = smn;
-			smells.add(smn);
 		}
 	}
 
-	public static void decrSmellNode() {
-		Iterator<SmellNode> it = smells.iterator();
+	// 상어의 현재 위치에 생성, 재사용
+	public static void createSmellNode() {
+		for (int i = 1; i < sharks.size(); i++) {
+			SharkNode shark = sharks.get(i);
+			Node node = arr[shark.x][shark.y];
+			
+			if(node.sharkNumber == 0) 
+				smells.add(node);
+			
+			node.sharkNumber = shark.sharkNumber;
+			node.time = setTime;
+		}
+	}
+
+	public static void decreaseSmellNode() {
+		Iterator<Node> it = smells.iterator();
 		while (it.hasNext()) {
-			SmellNode smn = (SmellNode) it.next();
-			if (smn.k >= 2)
-				smn.k--;
-			else {
-				arr[smn.x][smn.y] = new Node(smn.x, smn.y);
+			Node node = it.next();
+
+			if (node.time > 0)
+				node.time--;
+
+			if (node.time == 0) {
+				node.sharkNumber = 0;
 				it.remove();
 			}
 		}
 	}
 
-	public static boolean sharkMove(SharkNode sn, int check) {
-		int[] xRange = { -1, 1, 0, 0 };
-		int[] yRange = { 0, 0, -1, 1 };
-		List<Integer> nowdir = sn.moveList.get(sn.dir);
+	public static void removeDeadShark() {
+		for (int i = 1; i < sharks.size() - 1; i++) {
+			for (int j = i + 1; j < sharks.size(); j++) {
+				SharkNode shark1 = sharks.get(i);
+				SharkNode shark2 = sharks.get(j);
 
-		for (int dir : nowdir) {
-			int nx = sn.x + xRange[dir];
-			int ny = sn.y + yRange[dir];
-
-			if (check == 0) {
-				if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr.length) {
-					if (!(arr[nx][ny] instanceof SmellNode)) {
-						sn.x = nx;
-						sn.y = ny;
-						sn.dir = dir;
-						arr[nx][ny] = sn;
-						return true;
+				if (shark1.x == shark2.x && shark1.y == shark2.y) {
+					if (shark1.sharkNumber > shark2.sharkNumber) {
+						sharks.remove(shark1);
+						shark1.survive = false;
+					} else {
+						sharks.remove(shark2);
+						shark2.survive = false;
 					}
 				}
-			} else {
-				if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr.length) {
-					SmellNode smn = (SmellNode) arr[nx][ny];
-					if (sn.sharkNumber == smn.sharkNumber) {
-						sn.x = nx;
-						sn.y = ny;
-						sn.dir = dir;
-						arr[nx][ny] = sn;
-						return true;
-					}
+			}
+		}
+	}
+
+	public static boolean sharkMove(SharkNode shark, int flag) {
+		int[] xRange = { -1, 1, 0, 0 };
+		int[] yRange = { 0, 0, -1, 1 };
+		List<Integer> dirList = shark.moveList.get(shark.dir);
+
+		for (int dir : dirList) {
+			int nx = shark.x + xRange[dir];
+			int ny = shark.y + yRange[dir];
+
+			if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr.length) {
+				if (flag == 0 && arr[nx][ny].sharkNumber == 0) {
+					shark.x = nx;
+					shark.y = ny;
+					shark.dir = dir;
+					return true;
+				} else if (flag == 1 && arr[nx][ny].sharkNumber == shark.sharkNumber) {
+					shark.x = nx;
+					shark.y = ny;
+					shark.dir = dir;
+					return true;
 				}
 			}
 		}
@@ -178,44 +184,43 @@ public class _19237 {
 
 class Node {
 	int x, y;
+	int sharkNumber;
+	int time;
 
 	public Node(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public Node(int x, int y, int sharkNumber, int time) {
+		this.x = x;
+		this.y = y;
+		this.sharkNumber = sharkNumber;
+		this.time = time;
+	}
+
+	public String toString() {
+		return sharkNumber + "-" + time;
 	}
 }
 
 class SharkNode extends Node {
 	int sharkNumber;
 	int dir;
+	boolean survive;
 	List<List<Integer>> moveList = new ArrayList<>();
 
 	public SharkNode(int x, int y, int sharkNumber, int dir) {
 		super(x, y);
 		this.sharkNumber = sharkNumber;
 		this.dir = dir;
+		this.survive = true;
 
 		for (int i = 0; i < 4; i++)
 			moveList.add(new ArrayList<>());
 	}
 
-	public void setMove(int row, int col) {
+	public void setMoveList(int row, int col) {
 		moveList.get(row).add(col);
-	}
-	
-	public String toString() {
-		return (this.x+1) + " " + (this.y+1) + " sn" +sharkNumber + " " + dir;
-	}
-	
-}
-
-class SmellNode extends Node {
-	int sharkNumber;
-	int k;
-
-	public SmellNode(int x, int y, int sharkNumber, int k) {
-		super(x, y);
-		this.sharkNumber = sharkNumber;
-		this.k = k;
 	}
 }
