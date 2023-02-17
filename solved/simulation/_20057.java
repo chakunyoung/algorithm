51ecadc2a28c6ea43b32ed9e33c6849e17200229
chakunyoung <chakunyou@naver.com>
2023-02-17 21:06:30 +0900

    마법사 토네이도

--- solved/simulation/_20057.java
+++ solved/simulation/_20057.java
@@ -1,16 +1,17 @@
+package solved.simulation;
 import java.util.*;
 import java.io.*;
 
-// answer = 격자밖으로 나간 모래의 양
-// arr 범위를 벗어나면, 나간 것
+// answer = 격자밖으로 나간 모래의 양 ( arr 범위를 벗어나면, 나간 것)
 
-// 토네이도
+//  - 토네이도
 // 2번 움직이고 이동량 증가
 // 이동량 전부 소모하면, dir 변화
 
+// 시간 소요된 곳 -> 두군데서 모두 처리해줘야 하는 부분을 if else로 나눠, 한군데에서만 처리해 디버깅이 오래걸림
 
-public class Main {
-
+public class _20057 {
+	
 	static int[][] arr;
 	static int size;
 	static int answerSandAmount;
@@ -28,11 +29,8 @@ public class Main {
 			for (int j = 0; j < size; j++)
 				arr[i][j] = Integer.parseInt(st.nextToken());
 		}
-
 		torna();
-
 		System.out.println(answerSandAmount);
-
 	}
 
 	public static void torna() {
@@ -43,19 +41,11 @@ public class Main {
 		int mvdis = 1;
 		int mvdistemp = mvdis;
 
-		while (isMoveTornado(t)) {
-			
-			for (int i = 0; i < size; i++) {
-				System.out.println(Arrays.toString(arr[i]));
-			}
-			
+		while (isMoveTornado(t)) { // 이동하고 해당위치의 모래 흡수
 			// 모래량 계산
 			sandCalc(t);
 
-			// 토네이도 남은 모래 update
-			updateSandLoc(t);
-
-			// dir 회전로직
+			// 토네이도 회전 로직
 			mvdistemp--;
 			if (mvdistemp == 0) {
 				turn--;
@@ -77,28 +67,24 @@ public class Main {
 		if (nx >= 0 && ny >= 0 && nx < size && ny < size) { // 이동 가능
 			t.x = nx;
 			t.y = ny;
-			t.sandAmount = arr[nx][ny];
+			t.sandAmount = arr[nx][ny]; // 해당 위치의 모래 가져오기
 			arr[nx][ny] = 0;
-			System.out.println("sand amount update" + t.sandAmount + " " + nx + " " + ny + " ");
 		} else // 이동 불가능, 종료
 			return false;
 		return true;
 	}
 
-	// 이동하는 모래의 양 위치 * 비율
-	// 한칸 밀려나는 모래의 양 현재 모래의 양 - (위치 * 비율)
+	// 움직인 이후 모래 계산
 	public static void sandCalc(Tornado t) {
-		int dir = t.dir;
-
 		// 움직인 이후가 기준
 		int[][] sandPercent = { { -1, -1, 2, -1, -1 }, { -1, 10, 7, 1, -1 }, { 5, -1, -1, -1, -1 },
-				{ -1, 10, 7, 1, -1 }, { -1, -1, 2, -1, -1 } };
+				{ -1, 10, 7, 1, -1 }, { -1, -1, 2, -1, -1 }, };
 
 		int[][] tempSand = new int[5][5];
-		for (int k = 0; k < dir; k++) { // dir 에 맞게 회전
+		for (int k = 0; k < t.dir; k++) { // dir 에 맞게 회전
 			for (int i = 0; i < 5; i++) {
 				for (int j = 0; j < 5; j++) {
-					tempSand[(size - 1) - j][i] = sandPercent[i][j];
+					tempSand[(5 - 1) - j][i] = sandPercent[i][j];
 				}
 			}
 
@@ -109,41 +95,37 @@ public class Main {
 
 		// 모래량 계산
 		int totalSandAmount = t.sandAmount;
-		System.out.println(totalSandAmount);
 		for (int i = -2; i <= 2; i++) {
 			for (int j = -2; j <= 2; j++) {
-				int sandPercentX = i + 2;
+				int sandPercentX = i + 2; // sand index 맞추기
 				int sandPercentY = j + 2;
 
 				if (sandPercent[sandPercentX][sandPercentY] == -1)
 					continue;
 
-				int arrX = t.x + i;
-				int arrY = t.y + j;
+				// 흩날리는 모래량
+				int locSand = (int) (t.sandAmount * (sandPercent[sandPercentX][sandPercentY] * 0.01));
 
-				// 해당 위치의 모래량
-				int locSand = t.sandAmount * sandPercent[sandPercentX][sandPercentY] / 100;
+				// 토네이도 위치를 기준으로 조정
+				int arrX = t.x + i; 
+				int arrY = t.y + j; 
 
+				totalSandAmount -= locSand;
 				if (arrX >= 0 && arrY >= 0 && arrX < size && arrY < size) { // 기존 모래와 합쳐짐
-					arr[arrX][arrY] += locSand;
-					totalSandAmount -= locSand;
-				} else { // 격자 밖으로 나감
+					arr[arrX][arrY] += locSand; // 여기서 업데이트를 너무 쌔게한다.
+				} else { // 격자 밖으로 나가는 경우
 					answerSandAmount += locSand;
 				}
 			}
 		}
 
-		t.sandAmount = totalSandAmount;
-		System.out.println(answerSandAmount);
-	}
-
-	public static void updateSandLoc(Tornado t) {
+		// 남아있는 모래량 놓아주기
 		int nx = t.x + xrange[t.dir];
-		int ny = t.y + xrange[t.dir];
+		int ny = t.y + yrange[t.dir];
 		if (nx >= 0 && ny >= 0 && nx < size && ny < size) {
-			arr[nx][ny] = t.sandAmount;
+			arr[nx][ny] += totalSandAmount;
 		} else {
-			answerSandAmount += t.sandAmount;
+			answerSandAmount += totalSandAmount;
 		}
 	}
 }
@@ -157,5 +139,4 @@ class Tornado {
 		this.dir = dir;
 		this.sandAmount = prevSandAmount;
 	}
-
 }
